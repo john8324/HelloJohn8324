73d4581a1d1a41795f2dfebef60a8a62d131dacd
john chimp <ay12345678@gmail.com>
2013-05-02 18:17:35 +0800

    Add  sum  (1~999)

--- HelloJohn.c
+++ HelloJohn.c
@@ -1,7 +1,17 @@
 #include<stdio.h>
+#include<stdlib.h>
 int main()
 {
     printf("Hello John!!!!!\n");
     printf("This is github test XDDDD\n");
+    printf("I just want to practice it.......\n");
+    printf("XDDDDDD\n");
+    int sum=0,i;
+    for(i=0;i<1000;i++)
+    {
+		sum+=i;
+		printf("sum=  %d\n",sum);
+	}
+	system("pause");
     return 0;
 }
